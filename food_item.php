ac3e53faf95117f46a98cea7f1ef3eedba549893
vidyaarumugam <vidyaarumugam531@gmail.com>
2021-03-06 22:10:51 +0530

    add food view modified

--- food_item.php
+++ food_item.php
@@ -3,13 +3,75 @@ include("admin/config.php");
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<style>
+	body {
+		font-family: Okra,Helvetica,sans-serif;
+	}
 		.img {
 			padding: 10px;
 			width: 450px;
 			height: 250px;
 			object-fit: contain;
+		}
+
+		.card-container {
+			display: flex;
+			flex-direction: row;
+			flex-wrap: wrap;
+			justify-content: center;
+		}
+
+		.card {
+			width: 450px;
+			height: 400px;
+			padding: 0 13px 18px 0;
+		}
+
+		.res-image {
+			height: 60%;
+			width: 100%;
+			border-radius: 5px;
+			transform: none;
+			opacity: 1;
+			will-change: transform, opacity;
+			object-fit: cover;
+			transition: opacity 0.25s ease 0s, transform 0.25s ease 0s;
+			
+
+		}
+
+		.res-details {
+			margin-top: 10px;
+			display: flex;
+			flex-direction: column;
+		}
+
+		.res-name {
+			font-weight: bold;
+			font-size: 18px;
+		}
+
+		.res-rating {
+			font-size: 18px;
+			display: flex;
+			align-items: center;
+		}
+
+		.res-rating>.material-icons {
+			font-size: 22px;
+			margin-right: 6px;
+		}
+
+		.rating-value,
+		.res-cuisines,
+		.res-location {
+			color: rgb(79, 79, 79);
+		}
+
+		.res-details div {
+			padding-bottom: 3px;
 		}
 	</style>
 	<link rel="stylesheet" type="text/css" href="./css/food_item.css">
@@ -32,48 +94,50 @@ include("admin/config.php");
 		<div class="box">
 			<form method="POST">
 				<input type="text" name="tosearch" placeholder="Type....">
-				<button type="submit" name="search" value="submit" >Submit</button>
+				<button type="submit" name="search" value="submit">Submit</button>
 			</form>
 		</div>
 	</header><br>
 	<h3 align="center"><em>The easiest way to get your favourite food</em></h3><br>
+	<div class="card-container">
 
-	<?php
-	if (isset($_REQUEST['search']) != "") {
-		$valueToSearch = $_REQUEST['tosearch'];
-		$query = "SELECT * FROM `tbl_images` WHERE Name like '%" . $valueToSearch . "%' OR Type like '%" . $valueToSearch . "%'" ;
-		$search_result = mysqli_query($conn, $query);
-		while ($rows = mysqli_fetch_array($search_result)) {
-	
-			echo
+		<?php
+		if (isset($_REQUEST['search']) != "") {
+			$valueToSearch = $_REQUEST['tosearch'];
+			$query = "SELECT * FROM `tbl_images` WHERE Name like '%" . $valueToSearch . "%' OR Type like '%" . $valueToSearch . "%'";
+			$search_result = mysqli_query($conn, $query);
+			while ($rows = mysqli_fetch_array($search_result)) {
+
+				echo
 				'
-	<div class="column">
-	
-	<div><img src="data:image/jpeg;base64,' . base64_encode($rows['image']) . '" class = "img"/>
-	<h4><center>' . $rows['Name'] . '</center></h4>
-	<span><center>Price:<span>' . $rows['Price'] . '</center></span>
-    </div>
+	<div class="card">
+		<img src="data:image/jpeg;base64,' . base64_encode($rows['image']) . '" class = "res-image"/>	
+		<div class="res-details">			
+			<div class="res-name">' . $rows['Name'] . '</div>
+			<div class="rating-value">Rs.' . $rows['Price'] . '</div>
+    	</div>
 	</div>
 					';
-		}
-	} else {
-		$query = "SELECT * FROM tbl_images ORDER BY id ASC";
-		$result = mysqli_query($conn, $query);
-		while ($row = mysqli_fetch_array($result)) {
+			}
+		} else {
+			$query = "SELECT * FROM tbl_images ORDER BY id ASC";
+			$result = mysqli_query($conn, $query);
+			while ($rows = mysqli_fetch_array($result)) {
 
-			echo
+				echo
 				'
-	<div class="column">
-	
-	<div><img src="data:image/jpeg;base64,' . base64_encode($row['image']) . '" class = "img"/>
-	<h4><center>' . $row['Name'] . '</center></h4>
-	<span><center>Price:<span>' . $row['Price'] . '</center></span>
-    </div>
-	</div>
+			<div class="card">
+			<img src="data:image/jpeg;base64,' . base64_encode($rows['image']) . '" class = "res-image"/>	
+			<div class="res-details">			
+				<div class="res-name">' . $rows['Name'] . '</div>
+				<div class="rating-value">Rs.' . $rows['Price'] . '</div>
+			</div>
+		</div>
 					';
+			}
 		}
-	}
-	?>
+		?>
+	</div>
 </body>
 
 </html>
